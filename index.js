4d7378b73ccb137cdffe385ef4fd1b59809be6a0
Raulanthropos <ipsichias@yandex.com>
2024-09-10 22:36:20 +0300

    Fixed layout, added randomizer

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 const addCardBtn = document.getElementById("add-card");
 const cards = document.getElementById("cards");
+let cardsArray = [];
 const quotes = [
-    "Inki Pinki Ponki",
-    "Daddy Bought A Donkey",
-    "Donkey Dies",
-    "Daddy cries",
-]
-
-console.log(Object.keys(cards))
+  "Inki Pinki Ponki",
+  "Daddy Bought A Donkey",
+  "Donkey Dies",
+  "Daddy cries",
+];
 
 function addCard() {
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.innerHTML = `  <h3>Card ${Object.keys(cards).length + 1}</h3>
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.innerHTML = `  <h3>Card ${Object.keys(cardsArray).length + 1}</h3>
                         <img src="https://picsum.photos/200/200">
-                        <p>${quotes[Math.floor(Math.random() * quotes.length)]}</p>`;
-    cards.appendChild(card);
+                        <p>${
+                          quotes[Math.floor(Math.random() * quotes.length)]
+                        }</p>`;
+  cards.appendChild(card);
+  cardsArray.push(card);
+  console.log(cardsArray.length, cardsArray);
 }
 
+function randomizeArray() {
+  cardsArray.map((card) => {
+    return Math.floor(Math.random() * cardsArray.indexOf(card));
+  });
+  console.log(cardsArray);
+}
 
 // addCard.addEventListener("click", () => {
 //   const card = document.createElement("div");
